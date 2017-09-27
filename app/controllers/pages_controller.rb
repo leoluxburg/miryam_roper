f932f749b1814432e1cf7249be08fbf68365fcfb
Leonardo Luxburg <leo.von.luxburg@gmail.com>
2017-09-27 03:03:32 -0500

    details

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,5 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:home, :carrera, :biografia]
+  skip_before_action :authenticate_user!, only: [:home, :carrera, :biografia, :videos, :contact]
 
   def home
   end
@@ -10,4 +10,9 @@ class PagesController < ApplicationController
   def biografia
   end
 
+  def videos
+  end
+
+  def contact
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
 
-   get 'pages/biografia',to: 'pages#biografia'
-
-
-   get 'pages/carrera', to:'pages#carrera'
+  get 'pages/biografia',to: 'pages#biografia'
+  get 'pages/carrera', to:'pages#carrera'
+  get 'pages/videos', to:'pages#videos'
+  get 'pages/contact', to:'pages#contact'
 end
