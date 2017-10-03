55806725b51eb52885f2e2a0aee9271921e10077
Leonardo Luxburg <leo.von.luxburg@gmail.com>
2017-10-03 00:58:21 -0500

    letter model

---
+++ db/migrate/20171003055637_create_letters.rb
@@ -0,0 +1,11 @@
+class CreateLetters < ActiveRecord::Migration[5.0]
+  def change
+    create_table :letters do |t|
+      t.string :name
+      t.string :email
+      t.text :message
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170925022931) do
+ActiveRecord::Schema.define(version: 20171003055637) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "letters", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.text     "message"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
